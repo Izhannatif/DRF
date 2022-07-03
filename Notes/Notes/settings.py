9dcc5ef3a72c7740ecb4f044f92c58b1cd37033e
Izhann <izhan124@gmail.com>
2022-07-03 17:15:09 +0500

    commit

--- Notes/Notes/settings.py
+++ Notes/Notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fzttmkx6=r#3)ls(&pi0ly=nvxy^=(utdhwiwri7l26pfu@(7)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-notes-rest-api.herokuapp.com"]
 
 
 # Application definition
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
